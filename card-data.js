e702e1dd13e5ab3f1fb56de1c6ec4f9e01b7d609
dl-watson <dlarkinwatson@gmail.com>
2020-10-19 16:25:03 -0700

    basic html structure with comments

---
+++ card-data.js
@@ -0,0 +1,21 @@
+// data model goes here
+
+/* 
+
+DATA MODEL:
+
+an array of objects
+
+const majorArcana = [ {
+    id: 'magician',
+    name: 'The Magician',
+    image: url('...'),
+    interpretation: 'text text text',
+    category: 'major-arcana',
+    }, 
+    {
+
+    } 
+]
+
+*/
