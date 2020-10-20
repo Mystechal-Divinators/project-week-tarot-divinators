289378624659db983eae005b18237c0d816c2209
dl-watson <dlarkinwatson@gmail.com>
2020-10-20 16:49:51 -0700

    working in reading-room.js

--- reading-room/reading-room.js
+++ reading-room/reading-room.js
@@ -1,12 +1,22 @@
 import { renderCard } from '../utils/renderCard.js';
 import { nCards } from '../utils/logic.js';
-import { getLocalStorage } from '../utils/localStorage-utils.js';
-import { ONE_OR_THREE } from '../utils/constants.js';
+import { getLocalStorage, setLocalStorage } from '../utils/localStorage-utils.js';
+import { ONE_OR_THREE, CARDS } from '../utils/constants.js';
 
 const spreadSelection = Number(getLocalStorage(ONE_OR_THREE));
 const randomCard = nCards(spreadSelection);
 
-// go back to renderCard and update keywords and img src when added to data model
+let localStorageCards = getLocalStorage(CARDS) || [];
+
+// randomCard is an array
+// so pushing it into an array doesn't work correctly
+localStorageCards.push(randomCard);
+
+setLocalStorage(CARDS, localStorageCards);
+
+
+// go back to renderCard and update img src when added to data model
 randomCard.forEach(card => {
     renderCard(card);
 });
+
