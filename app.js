0a2501c78a7539d2139c5e8984af8b0696934e1d
dl-watson <dlarkinwatson@gmail.com>
2020-10-20 15:39:20 -0700

    added radio buttons to the home page that push the value of the user selection to localStorage; added notes to reading-room.js that outline next steps; made a constants.js file

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-// import functions and grab DOM elements
+import { ONE_OR_THREE } from './utils/constants.js';
+import { setLocalStorage } from './utils/localStorage-utils.js';
 
-// initialize state
+const beginButton = document.getElementById('begin-button');
 
-// set event listeners to update state and DOM
+beginButton.addEventListener('click', () => {
+    const oneOrThree = document.querySelector(':checked');
+
+    // set local storage with the value we get from the selected option
+    setLocalStorage(ONE_OR_THREE, oneOrThree.value);
+
+    // navigate user to the next page
+    // window.location.href = './reading-room/index.html';
+});
