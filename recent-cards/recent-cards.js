100398974b1580c83bc6344fd2c43daa834c1383
Franco Ortega <francopdx@gmail.com>
2020-10-20 08:31:35 -0700

    add the ability to clear local storage to the Clear button and add comments outlining other js needed on Recent page

--- recent-cards/recent-cards.js
+++ recent-cards/recent-cards.js
@@ -1,5 +1,21 @@
 const clearButton = document.querySelector('.clear-button')
 
+
+
+
+// List of potention JavaScript functions and other code \\
+
+//function to get local storage (that has recent reading cards).
+
+//function to render cards (that may have a findById function inside it).
+
+//a loop that works with the renderCard function to render each card in local storage; maybe the renderCard function goes inside the loop.
+
+
+
+
+//Button to clear local storage. This button includes a console.log that will need to be removed later.
 clearButton.addEventListener('click', () => {
     console.log('Clear button was clicked.');
+    localStorage.clear();
 })
