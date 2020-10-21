1d9052a7d7fb3440dc7bb7c9f0bcb3f2372a0b80
Franco Ortega <francopdx@gmail.com>
2020-10-21 15:16:56 -0700

    add timestamp to local storage data

--- reading-room/reading-room.js
+++ reading-room/reading-room.js
@@ -2,9 +2,13 @@ import { renderCard } from '../utils/renderCard.js';
 import { nCards } from '../utils/logic.js';
 import { getLocalStorage, setLocalStorage } from '../utils/localStorage-utils.js';
 import { ONE_OR_THREE, CARDS } from '../utils/constants.js';
+import { timestampMaker } from '../utils/timestamp.js'
 
 const spreadSelection = Number(getLocalStorage(ONE_OR_THREE));
 const randomCard = nCards(spreadSelection);
+const currentDate = timestampMaker();
+
+console.log(currentDate);
 
 // go back to renderCard and update img src when added to data model
 randomCard.forEach(card => {
@@ -15,17 +19,12 @@ let localStorageCards = getLocalStorage(CARDS) || [];
 
 for (let i = 0; i < randomCard.length; i++) {
     const oneCard = randomCard[i];
+
+    oneCard.timestamp = currentDate;
+
+console.log(oneCard);
+
     localStorageCards.push(oneCard);
-    
 }
 
 setLocalStorage(CARDS, localStorageCards);
-
-
-//Get timestamp
-const timeStamp = new Date();
-console.log(timeStamp);
-
-//Timestamp in milliseconds
-const timeStamp2 = Math.floor(Date.now() / 1000);
-console.log(timeStamp2);
