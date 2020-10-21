a67d27cd0f072bb65f716d5ff5e353b3be70e456
dl-watson <dlarkinwatson@gmail.com>
2020-10-21 15:50:49 -0700

    pushing test solutions to make a pull from main

--- dee-test-reading-room-copy/reading-room-copy.js
+++ dee-test-reading-room-copy/reading-room-copy.js
@@ -1,30 +1,60 @@
-import { renderCard } from '../utils/renderCard.js';
+// import { renderCard } from '../utils/renderCard.js';
 import { nCards } from '../utils/logic.js';
 import { getLocalStorage, setLocalStorage } from '../utils/localStorage-utils.js';
 import { ONE_OR_THREE, CARDS } from '../utils/constants.js';
 
-const spreadSelection = Number(getLocalStorage(ONE_OR_THREE));
-const randomCard = nCards(spreadSelection);
+const anchor = document.querySelector('section');
 
-// go back to renderCard and update img src when added to data model
-randomCard.forEach(card => {
-    renderCard(card);
-});
+function renderCard(card) {
+    const container = document.createElement('div');
+    const img = document.createElement('img');
+    const title = document.createElement('p');
+    const interpretation = document.createElement('div');
+    const keywords = document.createElement('div');
 
-let localStorageCards = getLocalStorage(CARDS) || [];
-
-for (let i = 0; i < randomCard.length; i++) {
-    const oneCard = randomCard[i];
-    localStorageCards.push(oneCard);
+    container.classList.add('card');
+    // container.classList.add('animation');
+    img.src = `../assets/major-arcana/${card.id}.png`;
+    title.textContent = card.name;
+    interpretation.textContent = card.interpretation;
+    keywords.textContent = card.keyWords;
+    
+    container.append(img, title, interpretation, keywords);
+    anchor.append(container);
 }
 
-setLocalStorage(CARDS, localStorageCards);
+function clickHandler() {
+    const spreadSelection = Number(getLocalStorage(ONE_OR_THREE));
+    const randomCard = nCards(spreadSelection);
+
+    randomCard.forEach(card => {
+        renderCard(card);
+    });
+
+    const generatedCards = document.getElementsByClassName('card');
+
+    generatedCards[0].classList.add('animation-one');
+    generatedCards[1].classList.add('animation-two');
+    generatedCards[2].classList.add('animation-three');
+    
+    let localStorageCards = getLocalStorage(CARDS) || [];
+    
+    for (let i = 0; i < randomCard.length; i++) {
+        const oneCard = randomCard[i];
+        localStorageCards.push(oneCard);
+    }
+    
+    setLocalStorage(CARDS, localStorageCards);
+}
+
+const onClick = () => {
+    clickHandler();
+    deck.removeEventListener('click', onClick);
+};
+
+const deck = document.querySelector('img');
+
+deck.addEventListener('click', onClick);
 
 
-//Get timestamp
-const timeStamp = new Date();
-console.log(timeStamp);
-
-//Timestamp in milliseconds
-const timeStamp2 = Math.floor(Date.now() / 1000);
-console.log(timeStamp2);
+// need to figure out how to disable click handler after one click
