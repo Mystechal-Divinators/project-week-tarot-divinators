73f851d239e030c401b9b778ca3f85f652bd648c
Franco Ortega <francopdx@gmail.com>
2020-10-21 15:52:57 -0700

    create renderRecentCard function in recent-cards js

--- recent-cards/recent-cards.js
+++ recent-cards/recent-cards.js
@@ -1,10 +1,41 @@
 import { getLocalStorage } from '../utils/localStorage-utils.js';
 import { CARDS } from '../utils/constants.js';
 import { renderCard } from '../utils/renderCard.js';
+import { timestampMaker } from '../utils/timestamp.js';
 
 
 const clearButton = document.querySelector('.clear-button');
 var paragraph = document.getElementById('message');
+
+//NEW STUFF START -Franco
+const currentDate = timestampMaker();
+
+console.log(currentDate);
+
+const anchor = document.querySelector('section');
+
+function renderRecentCard(card) {
+    const container = document.createElement('div');
+    const img = document.createElement('img');
+    const title = document.createElement('p');
+    const interpretation = document.createElement('div');
+    const keywords = document.createElement('div');
+
+    container.classList.add('card');
+    img.src = `../assets/major-arcana/${card.id}.png`;
+    title.textContent = card.name;
+    interpretation.textContent = card.interpretation;
+    keywords.textContent = card.keyWords;
+
+    container.append(img, title, interpretation, keywords);
+    anchor.append(container);
+}
+//NEW STUFF END -Franco
+
+
+
+
+
 
 //create variable to hold card array from local storage
 const recentCards = getLocalStorage(CARDS);
@@ -26,9 +57,11 @@ if (recentCards <= [0]) {
     for (let i = 0; i < recentCards.length; i++) {
         const oneRecentCard = recentCards[i];
 
-        renderCard(oneRecentCard);
+        renderRecentCard(oneRecentCard);
     }
 }
+
+
 
     
 //Button to clear local storage. This button includes a console.log that will need to be removed later.
@@ -38,3 +71,9 @@ clearButton.addEventListener('click', () => {
   
     window.location.reload();
 });
+
+
+
+
+//    interpretation.classList.add('hide');
+//    keywords.classList.add('cool');
